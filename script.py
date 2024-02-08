d1e96723a06597c542dbb9401eb32eaa08390595
dxia2p <d.xia111@gmail.com>
2024-02-08 09:59:13 -0700

    asdas

---
+++ script.py
@@ -0,0 +1,16 @@
+def MainMenu():
+    print("*** MAIN MENU ***")
+    print("* 1: Print List")
+    print("* 2: Add Item To End")
+    print("* 3: Remove Last Item")
+    print("* 4: Insert At Position")
+    print("* 5: Remove At Position")
+    print("* 6: Move To Position")
+    print("* 7: Edit Item")
+    print("* 8: Exit")
+    return input("* Enter Option #: ")
+
+inp = MainMenu()
+while(inp != 8){
+    
+}
